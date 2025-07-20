44350f248c836c08c9b3055e22317bf35fc03e0b
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-20 12:55:50 +0600

    create a sperate appbar client for user-app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -7,20 +7,5 @@ export default function Home() {
   const store = useStore((state) => state.balance);
   const { data: session } = useSession();
   console.log(session);
-  return (
-    <div className="text-red-900">
-      {" "}
-      <>
-        <div className="flex justify-center items-center z-40">
-          <div className=" flex justify-between items-center">
-            {session ? (
-              <button onClick={() => signOut()}>SignOut</button>
-            ) : (
-              <button onClick={() => signIn()}>login</button>
-            )}
-          </div>
-        </div>
-      </>
-    </div>
-  );
+  return <div className=""></div>;
 }
