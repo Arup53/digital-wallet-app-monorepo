279ca86897e6d2894b455f2545ee70cd2c05c34d
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-21 22:44:41 +0600

    add p2ptransfer schema in db and a create query in p2ptransfer server action

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import db from "@repo/db/client";
 const app = express();
 
+app.use(express.json());
+
 app.post("/hdfcWebhook", async (req, res) => {
   //TODO: Add zod validation here?
   //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+  console.log(req.body);
   const paymentInformation: {
     token: string;
-    userId: string;
+    userId: number;
     amount: string;
   } = {
     token: req.body.token,
@@ -19,7 +22,7 @@ app.post("/hdfcWebhook", async (req, res) => {
     await db.$transaction([
       db.balance.updateMany({
         where: {
-          userId: Number(paymentInformation.userId),
+          userId: paymentInformation.userId,
         },
         data: {
           amount: {
